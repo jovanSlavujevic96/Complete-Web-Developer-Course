d1d809ef3302e973e465422911e817d9516499e2
Jovan Slavujevic <jotzilla96@gmail.com>
2022-03-10 01:23:37 +0100

    - APIs Exercises pt.4

---
+++ 9-apis/04.postcode_finder/get_postal_code.php
@@ -0,0 +1,14 @@
+<?php
+    if ($_GET && array_key_exists('city', $_GET) && $_GET['city'] != "") {
+        require("../02.googlemaps_api/key_reader.php");
+
+        $geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?city=".$_GET['city']."&key=".$apiKey;
+        $file_headers = @get_headers($geocode_url);
+        if ($file_headers[0] != 'HTTP/1.1 404 Not Found' && $file_headers[0] != 'HTTP/1.1 400 Bad Request') {
+            echo json_encode(file_get_contents($geocode_url));
+        }
+        else {
+            die("Failed to get geocode. Reason: '".$file_headers[0])."'";
+        }
+    }
+?>
---
+++ 9-apis/04.postcode_finder/index.js
@@ -0,0 +1,32 @@
+function GetPostalCode(city_name) {
+    $.ajax({
+        url: "get_postal_code.php?city="+city_name,
+        type: "GET",
+        error: function() {
+            alert("Error on getting city postal code");
+        },
+        success: function(data) {
+            alert(data);
+        },
+    });
+}
+
+$('#myOutput').hide();
+
+$('#mySubmit').click(function(){
+    $('#myOutput').show();
+    $('#myOutput').addClass("alert");
+    var entered_city = $('#myCityInput').val();
+    if (entered_city == "") {
+        $('#myOutput').addClass("alert-danger");
+        $('#myOutput').removeClass("alert-success");
+        $('#myOutput').html("FAILURE");
+    }
+    else {
+        $('#myOutput').removeClass("alert-danger");
+        $('#myOutput').addClass("alert-success");
+        $('#myOutput').html("SUCCESS");
+
+        GetPostalCode(entered_city);
+    }
+});
